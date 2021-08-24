045b259417da7ef3c0a0e9dfc9fb9712fe831c85
samankey <71953671+samankey@users.noreply.github.com>
2021-08-24 14:24:24 +0900

    Add: work-status 조건걸기

---
+++ components/WorkStatus.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p class="status" :style="{ backgroundColor: color }">
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: '#8B98A7',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.status {
+  padding: 5px 9px 3px 9px;
+  border-radius: 999px;
+  color: #fff;
+  background-color: rgb(139, 152, 167);
+  font-size: 12px;
+  font-weight: 400;
+}
+</style>
